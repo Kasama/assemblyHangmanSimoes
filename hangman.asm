b33bdd3d9071a5e7d18311b766e88310ac698f0a
Kasama <ralegro@bol.com.br>
2015-03-28 17:52:26 -0300

    workin game

--- hangman.asm
+++ hangman.asm
@@ -10,7 +10,7 @@ startScreenl3  : string "  |                                     "
 startScreenl4  : string "  |                                     "
 startScreenl5  : string "  |                                     "
 startScreenl6  : string "  |                                     "
-startScreenl7  : string "  |===><====                            "
+startScreenl7  : string "  |===><===                             "
 startScreenl8  : string "  |                                     "
 startScreenl9  : string "  |                                     "
 startScreenl10 : string "  |                                     "
@@ -49,14 +49,14 @@ endScreenl10 : string "  |                                     "
 endScreenl11 : string "  |                                     "
 endScreenl12 : string "-----                                   "
 endScreenl13 : string "                                        "
-endScreenl14 : string "   <the word goes here>                 " ; pos 563 on the screen
+endScreenl14 : string "                                        " ; pos 563 on the screen
 endScreenl15 : string "                                        "
 endScreenl16 : string "           Game Over Newbie             "
-endScreenl17 : string "                                        "
-endScreenl18 : string "             Press Enter                "
-endScreenl19 : string "               to play                  "
-endScreenl20 : string "                again                   "
-endScreenl21 : string "                                        "
+endScreenl17 : string "           You are now dead             "
+endScreenl18 : string "                                        "
+endScreenl19 : string "             Press Enter                "
+endScreenl20 : string "               to play                  "
+endScreenl21 : string "                again                   "
 endScreenl22 : string "                                        "
 endScreenl23 : string "                                        "
 endScreenl24 : string "                                        "
@@ -65,6 +65,38 @@ endScreenl26 : string "  Used Letters:                         "
 endScreenl27 : string "                                        " ; pos 1084 on the screen
 endScreenl28 : string "                                        "
 endScreenl29 : string "                                        "
+;--------------------------------------------------------------}
+; Win Screen---------------------------------------------------{
+WinScreen    : string "                                        "
+WinScreenl1  : string "  +----+                                "
+WinScreenl2  : string "  |/   |                                "
+WinScreenl3  : string "  |    |                                "
+WinScreenl4  : string "  |    )                                "
+WinScreenl5  : string "  |                                     "
+WinScreenl6  : string "  |                                     "
+WinScreenl7  : string "  |===><===                             "
+WinScreenl8  : string "  |        =       O /                  "
+WinScreenl9  : string "  |         =     |H                    "
+WinScreenl10 : string "  |          =     U                    "
+WinScreenl11 : string "  |           =   | |                   "
+WinScreenl12 : string "-----          ============             "
+WinScreenl13 : string "                                        "
+WinScreenl14 : string "                                        " ; pos 563 on the screen
+WinScreenl15 : string "                                        "
+WinScreenl16 : string "        You guessed correctly!          "
+WinScreenl17 : string "                                        "
+WinScreenl18 : string "             Press Enter                "
+WinScreenl19 : string "               to play                  "
+WinScreenl20 : string "                again                   "
+WinScreenl21 : string "                                        "
+WinScreenl22 : string "                                        "
+WinScreenl23 : string "                                        "
+WinScreenl24 : string "                                        "
+WinScreenl25 : string "                                        "
+WinScreenl26 : string "  Used Letters:                         "
+WinScreenl27 : string "                                        " ; pos 1084 on the screen
+WinScreenl28 : string "                                        "
+WinScreenl29 : string "                                        "
 ;--------------------------------------------------------------}
 ;--------------------------------------------------------------
 
@@ -79,9 +111,12 @@ messageLetters : string "  Used Letters:                         "
 ; Global variables -------------------------------------------{
 TypedLetters : var #50 ; Array of typed letters
 TypedCounter : var #1  ; Number of typed letters
+static TypedCounter, #0; Initialize with zero
 Word : var #41         ; Typed Word
 WordSize : var #1      ; Word length
+static WordSize, #0	   ; initialize with zero
 WrongGuesses : var #1  ; Number of wrong guesses
+static WrongGuesses, #0; initialize with zero
 Score : var #1		   ; Player Score
 static Score + #0, #0  ; initialize with zero
 
@@ -96,7 +131,7 @@ static HangmanPos + #5, #246
 static HangmanPos + #6, #248
 ;-------------------------------------------------------------}
 
-;Main----------------------------------------------------------{
+; Main---------------------------------------------------------{
 main:
 	
 	; Initialize global variables {
@@ -320,7 +355,7 @@ gameLoop: ; the main game loop; void () ;{
 	push r7
 	
 	loadn r0, #255  ; load nothing
-	loadn r2, #' '  ; loadn space
+	loadn r2, #' '  ; load space
 	loadn r3, #0	; zero
 	gameloop_GetInput:
 		inchar r1
@@ -329,14 +364,16 @@ gameLoop: ; the main game loop; void () ;{
 	cmp r1, r2		; if space, read again
 	jeq gameloop_GetInput
 
-	call compareTypedLetterWithTrylist
-	ceq alreadyTypedMessage
-	jeq gameLoop_GetInput
-	call compareTypedLetterWithWord
-	cz hangTheMan
-	call checkGameOver
-	cnz gameOverScreen
-	jz gameloop_GetInput
+	call clearAlreadyTypedMessage		; clear the message for already typed
+	call compareTypedLetterWithTryList	; eq = already typed | ne = new letter
+	ceq alreadyTyped					; print message
+	jeq gameloop_GetInput				; go back to getting a letter
+	call updateTypedList				; update the list of typed letters
+	call compareTypedLetterWithWord		; eq = correct letter | ne = wrong
+	cne hangTheMan						; add a body part
+	call checkGameOver					; eq = game over | ne = game not over
+	ceq gameOverScreen					; show the game over screen
+	jmp gameloop_GetInput				; go back to reading a letter
 	
 	pop r7
 	pop r6
@@ -349,7 +386,7 @@ gameLoop: ; the main game loop; void () ;{
 	
 rts
 ;}
-
+;--------------------------------------------------------------
 compareTypedLetterWithTryList: ; compares the typedLetter with try list; fr = alreadyTyped(1 = true|0 = false) (r1 = typed letter) {
 
 	push r0
@@ -366,7 +403,7 @@ compareTypedLetterWithTryList: ; compares the typedLetter with try list; fr = al
 	loadn r3, #0
 	loadn r7, #0	; alreadyTyped flag
 	cmp r2, r3 ; compare with zero
-	jeq compareTypedLetterWithTrylist_Exit
+	jeq compareTypedLetterWithTrylist_PostLoop
 	
 	compareTypedLetterWithTrylist_Loop:
 		dec r2			; update i
@@ -376,6 +413,8 @@ compareTypedLetterWithTryList: ; compares the typedLetter with try list; fr = al
 		jeq compareTypedLetterWithTrylist_AlreadyTyped
 		cmp r2, r3		; compare with zero
 	jne compareTypedLetterWithTrylist_Loop
+
+	compareTypedLetterWithTrylist_PostLoop:
 	load r2, TypedCounter
 	add r4, r0, r2		; index to last posion
 	storei r4, r1		; save typed letter
@@ -402,7 +441,7 @@ compareTypedLetterWithTrylist_AlreadyTyped:
 jmp compareTypedLetterWithTrylist_Exit
 	
 ;}
-;------------------------------------------------------
+;--------------------------------------------------------------
 compareTypedLetterWithWord: ; compare the typed letter with word; fr = score(1 = true|0 = false) (r1 = typedLetter) {
 	push r0
 	push r1
@@ -444,37 +483,191 @@ rts
 
 compareTypedLetterWithWord_Scored:
 
+	push r0
+	push r1
+	loadn r0, #256 ; color brown
+	add r1, r1, r0 ; add color to the letter
+	loadn r0, #562 ; first position of the word
+	add r0, r0, r2 ; position of the letter's gap
+	outchar r1, r0 ; print the correct letter to fill the gap
 	inc r6 ;inc score
 	loadn r7, #1 ; set score flag true
+	pop r1
+	pop r0
 
 rts
 
 ;}
-;------------------------------------------------------
-	
+;--------------------------------------------------------------
 alreadyTyped: ; print message and update try list; void () {
-push r0
-push r1
-push r2
-push r3
-push r4
-push r5
-push r6
-push r7
-push fr
+	push r0
+	push r1
+	push r2
+	push fr
 
-;TODO
+	; print the key already typed message
+	loadn r0, #640					; line 16
+	loadn r1, #messageLetterTyped	; message address
+	loadn r2, #0					; color white
+	call printStr					; print the message
 
-pop fr
-pop r7
-pop r6
-pop r5
-pop r4
-pop r3
-pop r2
+	pop fr
+	pop r2
+	pop r1
+	pop r0
+
+rts
+;}
+;--------------------------------------------------------------
+clearAlreadyTypedMessage: ; clear the message; void () {
+	push r0
+	push r1
+	push r2
+
+	loadn r0, #640			; line 16
+	loadn r1, #messageBlank ; message address
+	loadn r2, #0			; color white
+	call printStr			; print the message
+
+	pop r2
+	pop r1
+	pop r0
+rts
+;}
+;--------------------------------------------------------------
+updateTypedList: ; update the letters on screen; void () {
+	push r0
+	push r1
+	push r2
+	
+	loadn r0, #1120			; load screen position
+	loadn r1, #TypedLetters ; load letters address
+	loadn r2, #512			; load color white
+	call printStr			; print the string until \0
+	
+	pop r2
+	pop r1
+	pop r0
+	
+
+rts
+;}
+;--------------------------------------------------------------
+hangTheMan: ; add a part of the body as the player fails to typea correct word; void () {
+
+	push r0
+	push r1
+	push r2
+	push r3
+	
+	loadn r0, #Hangman		; load the hangman parts
+	loadn r1, #HangmanPos	; load the hangman positions
+	load r2, WrongGuesses	; load the number of wrong guesses
+	loadn r3, #7			; load maximun number of wrong guesses
+	add r0, r0, r2			; update the pointer to the parts
+	add r1, r1, r2			; update the pointer to the positions
+	loadi r0, r0			; dereference the hangman part character
+	loadi r1, r1			; dereference the hangman position character
+	outchar	r0, r1			; print the part at the position
+	inc r2					; add a wrong guess
+	store WrongGuesses, r2	; save new number
+	
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+
+rts
+;}
+;--------------------------------------------------------------
+checkGameOver: ; check if the game is over; fr = gameOver(1 = true|0 = false) () {
+
+	push r0
+	push r1
+
+	load r0, WordSize
+	load r1, Score
+	cmp r0, r1
+	jeq checkGameOver_WinGame
+
+	loadn r0, #7			; load the maximum number of errors
+	load r1, WrongGuesses	; load current number of errors
+	cmp r0, r1				; compare both
+
+	pop r1
+	pop r0
+
+rts
+
+checkGameOver_WinGame:
 pop r1
 pop r0
 
+jmp gameOverScreenWinGame
+
+;}
+;--------------------------------------------------------------
+gameOverScreenWinGame: ; show the game over screen for a won game; void () {
+	
+	push r0
+	push r1
+	push r2
+
+	loadn r0, #WinScreen	; load the final win screen
+	call printScreen		; print the win screen
+
+	loadn r0, #563			; word position
+	loadn r1, #Word			; word address
+	loadn r2, #256			; word color (brown)
+	call printStr			; print the word
+	call updateTypedList	; update the typed list
+	call waitForEnter		; wait until a enter is pressed
+	call resetAllVariables	; reset all variables to restart the game
+
+	pop r2
+	pop r1
+	pop r0
+	pop r0
+	
+jmp main
+;}
+;--------------------------------------------------------------
+gameOverScreen: ; show the game over screen and aks to play again; void () {
+
+	push r0
+	push r1
+	push r2
+	
+	loadn r0, #endScreen	; load the final game over screen
+	call printScreen		; print the game over screen
+	loadn r0, #563			; word position
+	loadn r1, #Word			; word address
+	loadn r2, #256			; word color (brown)
+	call printStr			; print the word
+	call updateTypedList	; update the typed list
+	call waitForEnter		; wait until a enter is pressed
+	call resetAllVariables	; reset all variables to restart the game
+	
+	pop r2
+	pop r1
+	pop r0
+	pop r0
+
+jmp main
+;}
+;--------------------------------------------------------------
+resetAllVariables: ; restores initial state of all variables; void () {
+	push r0
+
+	loadn r0, #0
+	; initialize all variables with 0
+	store WrongGuesses, r0
+	store Word, r0
+	store WordSize, r0
+	store TypedCounter, r0
+	store Score, r0
+	
+	pop r0
 rts
 ;}
-;------------------------------------------------------
+;--------------------------------------------------------------
